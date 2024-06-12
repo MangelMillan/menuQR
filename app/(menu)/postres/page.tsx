b646beb98c90fce4636f2b5484e3136c415c9e7d
miakol77 <itzironartz@gmail.com>
2024-06-12 14:17:50 -0600

    pages fix

--- app/(menu)/postres/page.tsx
+++ app/(menu)/postres/page.tsx
@@ -5,36 +5,54 @@ export default function Comidas() {
   return (
     <div className="relative min-h-screen p-4">
       <BackButton />
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 items-center justify-center max-w-screen-lg mx-auto">
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 md:gap-28 lg:grid-cols-4 gap-4 w-full max-w-screen-xl mx-auto p-4 ">
         <FlipCard
           frontImageUrl="/entradas/entrada1.jpeg"
-          text="Pollo con Limón - $12.99"
-          backText="Pan tostado con ajo, cubierto con tomates frescos, albahaca y un toque de aceite de oliva."
+          text="Papas Secas"
+          backText="Camarones frescos marinados en jugo de limón con cilantro, cebolla y tomate."
+          price="$14.99"
         />
         <FlipCard
           frontImageUrl="/entradas/entrada2.jpeg"
-          text="Papas Secas - $8.99"
-          backText="Camarones frescos marinados en jugo de limón con cilantro, cebolla y tomate."
+          text="Pan con Queso y Hierba"
+          backText="Base crujiente rellena de una mezcla cremosa de champiñones y hierbas."
+          price="$9.99"
         />
         <FlipCard
           frontImageUrl="/entradas/entrada3.jpeg"
-          text="Pan con Queso y Hierba - $9.99"
-          backText="Base crujiente rellena de una mezcla cremosa de champiñones y hierbas."
+          text="Sushi de Pepino"
+          backText="Laminas de zucchini rellenas de queso ricotta, espinacas y hierbas, servidos con salsa de tomate."
+          price="$11.99"
         />
         <FlipCard
           frontImageUrl="/entradas/entrada4.jpeg"
-          text="Sushi de Pepino - $11.99"
-          backText="Laminas de zucchini rellenas de queso ricotta, espinacas y hierbas, servidos con salsa de tomate."
-        />
-        <FlipCard
-          frontImageUrl="/entradas/entrada5.jpeg"
-          text="Ensalada Caprese - $10.99"
+          text="Ensalada Caprese"
           backText="Rodajas de tomate y mozzarella fresca, con albahaca y un chorrito de aceite de oliva."
+          price="$10.99"
         />
         <FlipCard
-          frontImageUrl="/entradas/entrada6.jpeg"
-          text="Tostadas de Atún - $13.99"
-          backText="Tostadas crujientes cubiertas con atún fresco, aguacate, y un toque de salsa picante."
+          frontImageUrl="/entradas/entrada1.jpeg"
+          text="Papas Secas"
+          backText="Camarones frescos marinados en jugo de limón con cilantro, cebolla y tomate."
+          price="$14.99"
+        />
+        <FlipCard
+          frontImageUrl="/entradas/entrada2.jpeg"
+          text="Pan con Queso y Hierba"
+          backText="Base crujiente rellena de una mezcla cremosa de champiñones y hierbas."
+          price="$9.99"
+        />
+        <FlipCard
+          frontImageUrl="/entradas/entrada3.jpeg"
+          text="Sushi de Pepino"
+          backText="Laminas de zucchini rellenas de queso ricotta, espinacas y hierbas, servidos con salsa de tomate."
+          price="$11.99"
+        />
+        <FlipCard
+          frontImageUrl="/entradas/entrada4.jpeg"
+          text="Ensalada Caprese"
+          backText="Rodajas de tomate y mozzarella fresca, con albahaca y un chorrito de aceite de oliva."
+          price="$10.99"
         />
       </div>
     </div>
